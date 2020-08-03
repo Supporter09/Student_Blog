c1a891857ae2e147448c62b843b722fe5c756b54
TheCoder! <nhatminhgg2005@gmail.com>
2020-08-03 23:15:07 +0700

    UPDATE COMMENT FUNCTION

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+
 var Pusher = require('pusher');
 
 var pusher = new Pusher({
-    app_id: "1048586",
-    key : "1c9cb1b2261aaaa83806",
-    secret: "8efed3f362759dead511",
-    cluster: "ap1",
-    encrypted: true,
+  appId: '1048586',
+  key: '1c9cb1b2261aaaa83806',
+  secret: '8efed3f362759dead511',
+  cluster: 'ap1',
+  encrypted: true
 });
+
 var app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/comment', function(req, res){
+  console.log(req.body);
+  var newComment = {
+    name: req.body.name,
+    email: req.body.email,
+    comment: req.body.comment
+  }
+  pusher.trigger('flash-comments', 'new_comment', newComment);
+  res.json({  created: true });
+});
 
 // Error Handler for 404 Pages
 app.use(function(req, res, next) {
@@ -23,17 +37,6 @@ app.use(function(req, res, next) {
 });
 
 module.exports = app;
-
-app.post('/comment', function(req, res){
-    console.log(req.body);
-    var newComment = {
-      name: req.body.name,
-      email: req.body.email,
-      comment: req.body.comment
-    }
-    pusher.trigger('flash-comments', 'new_comment', newComment);
-    res.json({ created: true });
-  });
 
 app.listen(9000, function(){
   console.log('Example app listening on port 9000!')
